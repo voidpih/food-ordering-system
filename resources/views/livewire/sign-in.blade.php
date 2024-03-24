f80a772f63fa80feb807f87c28a2650a2abb23c2
apihdotpy <74187080+apihdotpy@users.noreply.github.com>
2024-03-24 20:16:18 +0800

    Update sign-in form to display error message for email validation

--- resources/views/livewire/sign-in.blade.php
+++ resources/views/livewire/sign-in.blade.php
@@ -1,4 +1,4 @@
-<form wire:submit="save">
+<form wire:submit="signIn">
     <div class="w-full py-1">
         <div class="flex flex-col gap-4">
             <div class="grid space-y-2">
@@ -8,6 +8,11 @@
             <div class="grid space-y-2">
                 <x-label for="email">Password</x-label>
                 <x-input type="password" placeholder="********" wire:model="password" />
+                <div>
+                    @error('email')
+                        <span>{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
             <div class="mt-4 grid">
                 <x-button type="submit">Sign in</x-button>
